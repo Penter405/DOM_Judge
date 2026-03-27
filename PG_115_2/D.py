6b26213eb1032f8f66e5d92040c547f229a7a020
Penter405 <bsid405@gmail.com>
2026-03-27 15:04:36 +0000

    static method , PG 115_2 D.py

---
+++ PG_115_2/D.py
@@ -0,0 +1,21 @@
+class Number:
+    def __init__(self):
+        pass
+    @staticmethod
+    def happy(num):
+        appear=set()
+        result=num
+        #appear.add(result)
+        buffer=0
+        while result not in appear:
+            appear.add(result)
+            buffer=0
+            for rs in str(result):
+                buffer+=int(rs)**2
+            result=buffer
+            if result==1:
+                return True
+
+
+        return False
+print(Number.happy(28))
