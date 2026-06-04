3c306a6ee8e633d2b3cd7745d05215d3de28c2a0
Penter405 <bsid405@gmail.com>
2026-06-04 12:06:38 +0000

    update logic about G_110 G.py

--- G_110/G.py
+++ G_110/G.py
@@ -2,23 +2,25 @@ from collections import Counter
 correct="".join((list(map(str,input().split()))))
 result=[]
 for _ in range(int(input())):
+    new_c=""
+    new_u=""
     a=0
     b=0
     user="".join((list(map(str,input().split()))))
     #print(correct,user)
     used=dict()
     for rs in range(4):
-        if user[rs] in used or correct[rs] in used:
-            continue
         if user[rs] ==correct[rs]:
             #print("same",correct[rs],user[rs])
-            used[user[rs]]=1
             a+=1
-    c_c=dict(Counter(correct))
-    u_c=dict(Counter(user))
+        else:
+            new_c+=correct[rs]
+            new_u+=user[rs]
+    c_c=dict(Counter(new_c))
+    u_c=dict(Counter(new_u))
     for rs in sorted(c_c):
-        if rs in u_c and rs not in used:
-            b+=1
+        if rs in u_c:
+            b+=min(c_c[rs],u_c[rs])
     result.append(f"{a}A{b}B")
 print("\n".join(result))
             
