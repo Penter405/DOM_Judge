1b45a11434173f9dae789e4ffa90fa5eb24cbfac
Penter405 <bsid405@gmail.com>
2026-06-05 08:01:45 +0000

    NTUB 115_SCI_DS A02

---
+++ NTUB/115_SCI_DS/A02.py
@@ -0,0 +1,19 @@
+useless=input()
+coin=list(map(int,input().split()))
+result=set()
+goal=int(input())
+def recursion(my_list,less):
+    #print(my_list)
+    if less==0:
+        result.add(str(tuple(my_list)))
+    if less<0:
+        return 0
+    for rs in range(len(coin)):
+        my_list2=my_list.copy()
+        my_list2[rs]+=1
+        recursion(my_list2,less-coin[rs])
+
+recursion([0]*len(coin),goal)
+for rs in sorted(result):
+    rs.replace(" ","")
+    print(rs)
--- NTUB/115_SCI_DS/help.py
+++ NTUB/115_SCI_DS/help.py
@@ -1,2 +1,1 @@
-for rs in sorted({}):
-    print("hi")
+print([0]*3)
