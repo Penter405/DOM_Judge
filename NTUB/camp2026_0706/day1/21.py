9e8b9d9c7c89b88f34c7829fb18d29c7b65c7983
Penter405 <bsid405@gmail.com>
2026-07-06 06:54:11 +0000

    camp2026_0706/day1/21.py

--- NTUB/camp2026_0706/day1/21.py
+++ NTUB/camp2026_0706/day1/21.py
@@ -1,5 +1,8 @@
 all, missed=list(map(int,input().split()))
-result=int((1+all)/2*all)-sum(list(map(int,input().split())))
+bot=0
+if missed!=0:
+    bot=sum(list(map(int,input().split())))
+result=int((1+all)/2*all)-bot
 if result>65535:
     print("PORT OVERFLOW")
 else:
