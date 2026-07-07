52d61026c78a89556ce7b723000923e6b7daf134
Penter405 <bsid405@gmail.com>
2026-07-07 04:52:41 +0000

    NTUB/camp2026_0706/day2/19.py

--- NTUB/camp2026_0706/day2/19.py
+++ NTUB/camp2026_0706/day2/19.py
@@ -6,10 +6,12 @@ def marge(a:list,b:list):
     while True:
         if pointer_a>=len(a) and pointer_b>=len(b):
             return result
-        if pointer_a>=len(a):
+        elif pointer_a>=len(a):
             result.extend(b[pointer_b:])
+            return result
         elif pointer_b>=len(b):
             result.extend(a[pointer_a:])
+            return result
         elif a[pointer_a]<b[pointer_b]:
             result.append(a[pointer_a])
             pointer_a+=1
@@ -17,4 +19,4 @@ def marge(a:list,b:list):
             result.append(b[pointer_b])
             pointer_b+=1
 input()
-print(marge(list(map(int,input.split())),list(map(int,input.split()))))
+print(" ".join(list(map(str,(marge(list(map(int,input().split())),list(map(int,input().split()))))))))
