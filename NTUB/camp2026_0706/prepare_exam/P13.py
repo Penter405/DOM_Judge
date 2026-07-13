b7b8fb322ba7f704ac25d23659658f3a558ef95c
Penter405 <bsid405@gmail.com>
2026-07-13 13:05:18 +0800

    NTUB\camp2026_0706\prepare_exam\P13.py

--- NTUB/camp2026_0706/prepare_exam/P13.py
+++ NTUB/camp2026_0706/prepare_exam/P13.py
@@ -2,7 +2,7 @@ result=[]
 for _ in range(int(input())):
     s=input()
     s2=s.split()
-    if s2[1]=="AM" and s2[0]!="12:00":   
+    if (s2[1]=="AM" and s2[0]!="12:00") or (s2[1]=="PM" and s2[0]=="12:00"):   
         result.append(s2[0])
     else:
         s2[0]=(str((int(s2[0][:2])+12)%24)).zfill(2)+s2[0][2:]
