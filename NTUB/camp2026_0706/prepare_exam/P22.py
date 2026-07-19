379a9be4d6d3ef6fa809ca4212a9c4515a479255
Penter405 <bsid405@gmail.com>
2026-07-19 23:35:57 +0800

    NTUB\camp2026_0706\prepare_exam\P22.py

--- NTUB/camp2026_0706/prepare_exam/P22.py
+++ NTUB/camp2026_0706/prepare_exam/P22.py
@@ -1,1 +1,22 @@
-print(0b1101 and 0b0011)
+def do(a,b):
+    result=[]
+    for rs in range(len(a)):
+        if a[rs]==b[rs]:
+            result.append(a[rs])
+        else:
+            if a[rs]==0:
+                result.append('1')
+            result.append('0')
+    return ''.join(result)
+
+data=input().split('/')
+binned=[]
+result=[]
+for rs in range(2):
+    binned.append([])
+    for pe in data[rs].split('.'):
+        binned[-1].append(bin(int(pe))[2:])
+for rs in range(4):
+    result.append(str(int(do(binned[0][rs],binned[1][rs]).zfill(8),2)))
+print('.'.join(result))
+#192.168.10.65/255.255.255.224
