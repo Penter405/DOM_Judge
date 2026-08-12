3f1ed09a0104817e07bbbe7558085bd3062e7416
Penter405 <bsid405@gmail.com>
2026-08-12 05:42:18 +0000

    LAB337.cpp

--- exam_2026y8m12d/LAB337.cpp
+++ exam_2026y8m12d/LAB337.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+int im_correct=0;
 array<int,3> guys;
-set<array<int,3>> ever;
-vector<array<int,3>> well;
-array<array<int,6>,3> take={{{2,1,0,1,0,0},{1,1,1,0,1,0},{0,2,3,0,0,1}}};
-int recursion(array<int,3> me,array<int,3> times){
+set<array<int,3>> ever_different;
+vector<array<int,2>> well;
+array<array<int,6>,3> take={{{2,1,0,1,0},{0,2,3,0,1}}};
+array<int,3> format_diff(array<int,3> me){
+    int min=me[0];
+    for(auto& it:me){
+        if(it<min){
+            min=it;
+        }
+    }
+    for(auto& it:me){
+        it-=min;
+    }
+    return me;
+}
+int recursion(array<int,3> me,array<int,2> times){
+    if(im_correct==1){
+        return 0;
+    }
     int good=1;
     for(auto& it:me){
         //cout<<it;
@@ -20,26 +36,41 @@ int recursion(array<int,3> me,array<int,3> times){
     if(good==-1){
         return 0;
     }else if(good==1){
-        well.push_back(times);
+        im_correct=1;
         return 0;
     }
-
-    if(ever.find(times)!=ever.end()){
+    array<int,3> fake=format_diff(me);
+    if(ever_different.find(fake)!=ever_different.end()){
         return 0;
     }
+    int local_correct=1;
+    for(auto it: fake){
+        if(it>0){
+            local_correct=0;
+            break;
+        }
 
-    ever.insert(times);
-    for(int x=0;x<3;++x){
+    }
+    if(local_correct){
+        im_correct=1;
+        return 0;
+    }
+    ever_different.insert(fake);
+    for(int x=0;x<2;++x){
         array<int,3> newme=me;
-        array<int,3> newtimes=times;
+        array<int,2> newtimes=times;
         for(int y=0;y<3;++y){
             newme[y]-=take[x][y];
+            
+        }
+        for(int y=0;y<2;++y){
             newtimes[y]+=take[x][y+3];
         }
         recursion(newme,newtimes);
     }
     return 0;
 }
+/*
 int gets(){
     for(auto rs:well){
         if(rs[0]*2+rs[1]==guys[0] && rs[0]+rs[1]+rs[2]*2==guys[1] && rs[1]+rs[2]*3==guys[2]){
@@ -48,6 +79,7 @@ int gets(){
     }
     return 0;
 }
+*/
 int main(){
     for(int x=0;x<3;++x){
         int bot;
@@ -58,8 +90,8 @@ int main(){
         }
         guys[x]=bot;
     }
-    recursion(guys,{0,0,0});
-    if(gets()==1){
+    recursion(guys,{0,0});
+    if(well.size()>0){
         cout<<"YES";
     }else{
         cout<<"NO";
